d9a4eee1f08f7a9d1dd7fe58e8ab4abe4e0d4adc
Corey Bertelsen <crbertelsen@gmail.com>
2023-05-14 22:16:22 +0200

    kap adv 4.2 min 4:30

--- KadenzeAdvancedAudioPlugin/Source/KAPLookAndFeel.h
+++ KadenzeAdvancedAudioPlugin/Source/KAPLookAndFeel.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "JuceHeader.h"
+#include "KAPInterfaceDefines.h"
 
 class KAPLookAndFeel :
     LookAndFeel_V4
@@ -18,6 +19,24 @@ class KAPLookAndFeel :
 public:
     KAPLookAndFeel();
     ~KAPLookAndFeel();
+
+    Font getTextButtonFont(TextButton&, int buttonHeight) override
+    {
+        return font_1;
+    }
+    void drawButtonBackground(Graphics&, Button&, const Colour& backgroundColour, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+    {
+        Colour fillColour;
+        if (shouldDrawButtonAsDown) 
+        {
+            fillColour = KAPColour_5;
+        }
+        else if (shouldDrawButtonAsHighlighted)
+        {
+            fillColour = KAPColour_3;
+        }
+    }
+
 private:
 
 };

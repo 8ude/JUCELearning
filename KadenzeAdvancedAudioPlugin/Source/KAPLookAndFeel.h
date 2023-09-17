ab443f98e98b50b87a37c2a0a8b0423afede9ee1
Corey Bertelsen <crbertelsen@gmail.com>
2023-09-17 21:28:34 +0200

    Kad Adv 4.2 Min 6:40

--- KadenzeAdvancedAudioPlugin/Source/KAPLookAndFeel.h
+++ KadenzeAdvancedAudioPlugin/Source/KAPLookAndFeel.h
@@ -24,7 +24,7 @@ public:
     {
         return font_1;
     }
-    void drawButtonBackground(Graphics&, Button&, const Colour& backgroundColour, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+    void drawButtonBackground(Graphics& g, Button& button, const Colour& backgroundColour, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
     {
         Colour fillColour;
         if (shouldDrawButtonAsDown) 
@@ -35,6 +35,14 @@ public:
         {
             fillColour = KAPColour_3;
         }
+
+        const float cornerSize = 6.0f;
+
+        const juce::Rectangle<float> bounds = button.getLocalBounds().toFloat().reduced(0.5f, 0.5f);
+
+        g.setColour(fillColour);
+        g.fillRoundedRectangle(bounds.reduced(1), cornerSize);
+
     }
 
 private:
